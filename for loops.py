eb53d9217a16b6a6dde85f75faaa57e2ea8d085e
roshanmustaqeem <roshanmustaqeem60@gmail.com>
2026-08-15 22:26:43 +0530

    commit

---
+++ for loops.py
@@ -0,0 +1,33 @@
+#Example 1
+# for day in range(1, 8):
+#     workout = input(f"Enter the workout for Day {day}: ")
+#     print(f"Workout for Day {day}: {workout}")
+#Example 2
+# word = "Roshan mustageem"
+# x = ("a", "e", "i", "o", "u")
+
+# for letter in word:
+#     if letter in x:
+#         print(letter)
+
+#Example 3
+# n = int(input("enter a number to get the sum: "))
+# sum = 0
+
+# for x in range(1, n + 1):
+#     sum += x
+
+# print(sum)
+
+#Continue and Break
+# for i in range(1, 10):
+#     if i == 6:
+#         continue
+#     else:
+#         print(i)
+
+# for i in range(1, 10):
+#     if i == 6:
+#         break
+#     else:
+#         print(i)
