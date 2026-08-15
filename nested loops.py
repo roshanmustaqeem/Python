3b4cf9657d73a6ae600360b8977a528229b0d0a3
roshanmustaqeem <roshanmustaqeem60@gmail.com>
2026-08-15 22:35:51 +0530

    commit

---
+++ nested loops.py
@@ -0,0 +1,46 @@
+# nested loop 
+# x = int(input("how many shelves you have: "))
+# y = int(input("how many books you have: "))
+
+# for i in range(1, x + 1):
+#     for j in range(1, y + 1):
+#         print(f"shelf {i} - book {j}")
+
+# print()
+
+#Project: Food Ordering Cart
+
+# foods = []
+# prices = []
+# total = 0
+
+# while True:
+#     food = input("enter the foods you like to order and q to quit: ")
+
+#     if food.lower() == "q":
+#         break
+#     else:
+#         price = float(input("enter the price of the food: $"))
+#         foods.append(food)
+#         prices.append(price)
+
+# print("--- your cart ---")
+
+# for food in foods:
+#     print(food, end=" ")
+
+# for price in prices:
+#     total = total + price
+
+# print()
+# print(f"your total is {total}")
+
+#2d collection
+
+# grocery = [["pizza", "juice", "momos"], ["carrot", "capsicum"], ["apple", "banana"]]
+
+# for collection in grocery:
+#     for food in collection:
+#         print(food, end=" ")
+#     print()
+
