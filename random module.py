3642c997f25fc1f46e7368c26f78c51ea7ddc488
roshanmustaqeem <roshanmustaqeem60@gmail.com>
2026-08-16 08:41:21 +0530

    Initial commit

---
+++ random module.py
@@ -0,0 +1,17 @@
+#Random numers 
+# for integer 
+import random
+#integer 
+# number=random.randint(1,12) 
+# print(number)
+#flot
+# number=random.random()
+# print(number)
+#choice
+# options=("rock","paper","scissors")
+# cho=random.choice(options)
+# print(cho)
+#shufffle 
+cards=[1,2,3,4,5,6,7,8,9,10,"j","k","q","a"]
+random.shuffle(cards)
+print(cards)
