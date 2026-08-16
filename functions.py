7d0e56b775609ae2784a882c98fb08415c17eb64
roshanmustaqeem <roshanmustaqeem60@gmail.com>
2026-08-16 19:54:24 +0530

    commit

---
+++ functions.py
@@ -0,0 +1,17 @@
+# def even(a,b):
+#  for c in range(a,b+1):
+#     d=c%2
+#     if d==0:
+#      print(c) 
+
+# even(10,20)
+
+# def add(num):
+#  total=0
+#  while num>0:
+#     c=num % 10
+#     total=total+c
+#     num//= 10
+#  return total
+
+# print(add(251))
