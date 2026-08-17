7006d08d416e40c4851c16877532921001a60fc5
roshanmustaqeem <roshanmustaqeem60@gmail.com>
2026-08-17 12:38:33 +0530

    commited

---
+++ iterabels.py
@@ -0,0 +1,23 @@
+#iterabelas, are the ojects that can return its element one at a time. 
+#"1.list"
+# alp=["a","b","c","d"]
+# for a in alp:
+#     print(a,end=" ")
+
+#2.sets
+# alp={"a","b","c","d"}
+# for a in alp:
+#     print(a,end=" ")
+
+#3.strings
+# alp="hello world"
+# for a in alp:
+#    print(a,end="") 
+   
+#dictionary 
+# info={
+#     "name":"roshan",
+#     "age":19
+# }
+# for i in info.values():
+#     print(i)
